33d0388d909421ab48ea29e2da03c649b20d1e08
kobigas <kobigas2007@gamil.com>
2025-07-08 10:57:39 +0530

    Register

---
+++ src/sec/com/java/workshop/Registration.java
@@ -0,0 +1,16 @@
+package src.sec.com.java.workshop;
+
+import java.time.LocalTime;
+
+public class Registration {
+    public static void main(String[] args) {
+        LocalTime currentTime=LocalTime.now();
+        LocalTime RegistrationLocalTime=LocalTime.of(18, 59, 55);
+        int hoursLeft=RegistrationLocalTime.getHour()-currentTime.getHour();
+        int minuteLeft=RegistrationLocalTime.getMinute()-currentTime.getMinute();
+        int secondLeft=RegistrationLocalTime.getSecond()-currentTime.getSecond();
+        System.out.println("You have "+hoursLeft +"hours, "+minuteLeft +"minutes, "+secondLeft +"seconds,");
+        
+    }
+
+}
