0cd5848033f408d0d3ac830ff6a0b1db1627b761
kobigas <kobigas2007@gamil.com>
2025-07-08 10:58:08 +0530

    Currentage

---
+++ src/sec/com/java/workshop/DOB.java
@@ -0,0 +1,16 @@
+package src.sec.com.java.workshop;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class DOB {
+    public static void main(String[] args) {
+    Scanner inputScanner = new Scanner(System.in);
+    System.out.println("Enter your Date of birth(YYY-MM-DD):");
+    String DateOfBirth=inputScanner.nextLine();
+    LocalDate birthDate=LocalDate.parse(DateOfBirth);
+    int YearOfBirth=birthDate.getYear();
+    int currentYear=LocalDate.now().getYear();
+    int age=currentYear-YearOfBirth;
+    System.out.println("Your age is:"+age);
+    }
+}
